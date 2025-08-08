84470f2fd461ce48a1dc669dcb8082354b9c0c32
Harsh Deep Singh <harshdeepsingh1554@gmail.com>
2025-08-08 20:52:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -109,6 +109,7 @@ def add_quote():
     return render_template('add_quote.html')
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))  # Render sets PORT env variable
-    app.run(host="0.0.0.0", port=port)e)
+    app.run(host="0.0.0.0", port=port)
+
 
 
